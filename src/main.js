ae4abd6ec26c95149a44cf1e680caf8b262006ef
yuandongx <786018072@qq.com>
2020-12-10 14:51:36 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from "./App.vue";
 //import "ant-design-vue/dist/antd.css";
 import { Layout,
          Menu,
-         Button } from "ant-design-vue";
+         Button 
+        } from "ant-design-vue";
 //import {} from "@ant-design/icons-vue"
 createApp(App).use(Layout).use(Menu).use(Button).mount("#app");
 
